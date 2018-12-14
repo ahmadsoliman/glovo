4932676168bb337b84acea998082ac0c065cb1f4
ahmadsoliman <ahmad.soliman@thoughtdesign.com.au>
2018-12-14 17:41:56 +0200

    Created /Products route

--- backend/server.js
+++ backend/server.js
@@ -7,8 +7,53 @@ import { config } from './config.js';
 const app = express();
 const router = express.Router();
 
+const request = require('request');
+const async = require('async');
+
+
 app.use(cors());
 app.use(bodyParser.json());
 
 app.use('/', router);
 app.listen(4000, () => console.log(`Express server running on port 4000`));
+
+// Set this array with exchanges needed for comparison
+const exchanges = config.exchanges;
+
+router.route('/products').get((req, res) => {
+  
+  async.map(exchanges, moneedaHttpGetProducts, function (err, allProducts){
+    if (err) return console.log(err);
+
+    // map all products to their name
+    for(let i=0; i<exchanges.length; i++) {
+      allProducts[i] = allProducts[i].map((product) => product.id);
+    }
+
+    // calculates the intersection of common products between all exchanges
+    let products = allProducts[0] ? allProducts[0] : [];
+    for(let i=1; i<exchanges.length; i++) {
+      products = products.filter(product => allProducts[i].includes(product));
+    }
+    
+    res.json(products);    
+  });
+
+});
+
+
+// helper function gets all products on an exchange
+function moneedaHttpGetProducts(exchange, callback) {
+  const options = {
+    url : config.baseUrl + exchange + '/products',
+    json : true,
+    auth : {
+      'bearer': config.authToken
+    }
+  };
+  request(options,
+    function(err, res, body) {
+      callback(err, body);
+    }
+  );
+}
